250784a3ea15d4a846ae05a39f5c44abee7e842d
jose-valdez12 <90970578+jose-valdez12@users.noreply.github.com>
2022-04-30 16:02:17 -0600

    clase2

--- TransaccionBancarias/src/transaccionbancarias/tarjetaCredito.java
+++ TransaccionBancarias/src/transaccionbancarias/tarjetaCredito.java
@@ -9,6 +9,37 @@ package transaccionbancarias;
  * @author jvald
  */
 public class tarjetaCredito {
+protected int transacciones , retiro , deposito ,saldo, salir;
+float fondos =5000;
 
+
+//Mostrar fondos 
+public int saldo (int fondos ){
+   return  fondos ;
+   }
+
+
+// Solicitar cuánto se retira
+public int retirar (int retiro, int fondos){
+  System.out.println("ingrese la cantidad a retirar ");
+  return fondos-retiro; 
+    }
+
+//solicitar transaccion 
+public int envio (int transacciones, int fondos){
+    System.out.println("de cuanto sera la transaccion");
+    return transacciones-fondos;
+}
+
+// solicitar deposito 
+public int ingreso (int deposito,int fondos){
+    System.out.println("ingrese cuanto desea depositar ");
+    return deposito+fondos;
+}
+
+public int salida (int salir ){
+    System.out.println("gracias por su consulta");
+    return salir ;
+}
         
 }
